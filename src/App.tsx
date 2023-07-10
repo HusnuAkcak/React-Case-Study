1a483ebb148df934af41254c21380e9cbad3bc73
hsn <hsn@DESKTOP-DK42F36>
2023-07-10 06:37:23 +0300

    grid component and control input elements are added with their bindings.

--- src/App.tsx
+++ src/App.tsx
@@ -3,14 +3,59 @@ import './style.css';
 import Grid from './grid';
 import dataList from './data.json';
 
-function control(today: Date, limit: number) {}
+function control(today: Date, limit: number){
+  return 0;
+}
 
 export default function App() {
   let sourceProp = dataList;
+  const [today, setToday] = React.useState(new Date());
+  const [limit, setLimit] = React.useState(4);
+  const [erroneousRowNumber, setErroneousRowNumber] = React.useState(0);
+
   return (
     <div>
       <h1>Dgpays Case Study </h1>
-      {/* <Grid source={sourceProp} />  */}
+      <Grid source={sourceProp} /> 
+
+      <br/>
+      <label>
+        Today: &emsp;
+        <input 
+          id="today" 
+          type="date" 
+          onChange={e => setToday(new Date(e.target.value))}>
+        </input>
+      </label>
+
+      <br></br>
+      <br></br>
+
+      <label>
+        Limit: &emsp;
+        <input 
+          id="input-limit" 
+          type="number" 
+          onChange={e => setLimit(Number(e.target.value))}>
+        </input>
+      </label>
+
+      <br></br>
+      <br></br>
+
+      <button 
+        id="calculate-erroneous-row-number"
+        onClick={e => setErroneousRowNumber(control(today, limit))}
+        >
+          Calculate Erroneous Row Number
+      </button>
+
+      <br></br>
+      <br></br>
+
+      <label>
+        Number of rows that are marked with the wrong color is {erroneousRowNumber}
+      </label>
     </div>
   );
 }
---
+++ src/grid.tsx
@@ -0,0 +1,28 @@
+import * as React from 'react';
+import { SubmitRecord } from "./submitRecord";
+
+const Grid = (props : {source: Array<SubmitRecord>} )=> {
+
+    return (
+        <React.Fragment>
+            <table id="submit-record-table">
+                <tbody>{
+                    props.source.map((item:SubmitRecord) => {
+                        return convertSubmitRecordToTableRow(item);
+                    })
+                }</tbody>
+            </table>
+            
+        </React.Fragment>
+    )
+};
+
+function convertSubmitRecordToTableRow(item:SubmitRecord){
+    return <tr className = {item.isBackgroundColorRed ? 'red-background' : ''}>
+        <td>{item.name}</td>
+        <td>{item.mailReceivedDate}</td>
+        <td>{item.solutionSentDate}</td>
+    </tr>
+}
+
+export default Grid;
